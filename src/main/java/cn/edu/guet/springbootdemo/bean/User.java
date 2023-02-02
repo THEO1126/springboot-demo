e0cb0f6f4f8ca6a0a6afa55ef18f468878ec6804
THEO1126 <1223230728@qq.com>
2023-02-03 03:12:54 +0800

    oa数据

--- src/main/java/cn/edu/guet/springbootdemo/bean/User.java
+++ src/main/java/cn/edu/guet/springbootdemo/bean/User.java
@@ -1,4 +1,7 @@
 package cn.edu.guet.springbootdemo.bean;
+
+import java.util.List;
+
 /**
  * @Author 李冰冰
  * @Date 2023/1/30
@@ -10,20 +13,26 @@ public class User {
     private int userId;
     private String username;
     private String password;
-//    private List<Role> roleList;
-    private Role role; // 一个管理员只对应一个角色
+    private List<Role> roleList;
+
 
     public User() {
     }
 
-//    public User(int userId, String username, List<Role> roleList) {
-//        this.userId = userId;
-//        this.username = username;
-//        this.roleList = roleList;
-//    }
+    public User(int userId, String username, List<Role> roleList) {
+        this.userId = userId;
+        this.username = username;
+        this.roleList = roleList;
+    }
 
 
+    public List<Role> getRoleList() {
+        return roleList;
+    }
 
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
     public int getUserId() {
         return userId;
     }
@@ -40,20 +49,6 @@ public class User {
         this.password = password;
     }
 
-//    public Role getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(Role role) {
-//        this.role = role;
-//    }
-//
-//    public User(int userId, String username, String password, Role role) {
-//        this.userId = userId;
-//        this.username = username;
-//        this.password = password;
-//        this.role = role;
-//    }
 
     public String getUsername() {
         return username;
@@ -63,11 +58,5 @@ public class User {
         this.username = username;
     }
 
-//    public List<Role> getRoleList() {
-//        return roleList;
-//    }
-//
-//    public void setRoleList(List<Role> roleList) {
-//        this.roleList = roleList;
-//    }
+
 }
